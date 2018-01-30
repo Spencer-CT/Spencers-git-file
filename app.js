a69fc4bfa3554983a2e2c54e13b525add1253da2
Spencer Thode <spencerthode@gmail.com>
2018-01-30 14:26:48 -0800

    completed js for day 2

---
+++ app.js
@@ -0,0 +1,56 @@
+
+const name      = prompt("What is your favorite code camp?");
+            console.log("Their favorite is: " + name);
+
+        if  (name.toLowerCase() === "alchemy code lab"){
+                alert("Hey, me too!");
+            }else{
+                alert("Then why are you here?")
+            }
+
+
+
+const header    = prompt("Are the Green Bay Packers your favorite FNL team?");
+            console.log(header);
+
+        if (header.toLowerCase() === "yes"){
+
+            alert("Well yeah of course they are!")
+            }else{
+                alert("I knew you had bad taste")
+            }
+
+
+const show      = prompt("Have you seen the show Black Mirror?");
+            console.log(show);            
+
+        if  (show.toLowerCase() === "yes"){
+
+            alert("It\'s good right! but dont watch it before bed!")
+            }else{
+                alert("Don\'t watch it it\'s scary!!")
+            }
+            
+    
+    
+const tea       = prompt("Do you drink Yerba Mate?");
+            console.log(tea);
+
+        if (tea.toLowerCase() === "yes"){
+
+            alert("I\'m all hyped out on caffeine!!");
+        }else{
+            alert("It gets you hyped on caffeine!!" )
+        }
+
+const music     = prompt("Do you listen to ambient music when falling asleep?");
+            console.log(music);
+        
+        if (music.toLowerCase() === "yes"){
+
+            alert("STILL HYPED ON CAFFEINE!!");
+        }else{
+            alert("It\'s relaxing you should try it...")
+        }
+
+        //pageYOffset
