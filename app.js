7255633d141111b7274aeb91f3f5b6302664b859
Spencer Thode <spencerthode@gmail.com>
2018-01-30 16:55:16 -0800

    same as prior except JS isnt commented out

--- app.js
+++ app.js
@@ -1,72 +1,72 @@
 
-// const name      = prompt("Do you enjoy being at Alchemy Code Lab?");
-//             console.log(name);
+const name      = prompt("Do you enjoy being at Alchemy Code Lab?");
+            console.log(name);
 
-//         if  (name.toLowerCase() === "yes" || name.toLowerCase() === "y"){
-//                 alert("Hey, me too!");
+        if  (name.toLowerCase() === "yes" || name.toLowerCase() === "y"){
+                alert("Hey, me too!");
 
-//         }else if (name.toLowerCase() === "no" || name.toLowerCase() === "n"){
-//             alert("Then why are you here?")
+        }else if (name.toLowerCase() === "no" || name.toLowerCase() === "n"){
+            alert("Then why are you here?")
 
-//             }else{
-//                 alert("Time to code or get off the pot!")
-//             }
-
-
-
-// const header    = prompt("Are the Green Bay Packers your favorite NFL team?");
-//             console.log(header);
-
-//         if (header.toLowerCase() === "yes" || header.toLowerCase() === "y"){
-//             alert("Well yeah of course they are!")
-
-//         }else if (header.toLowerCase() === "no" || header.toLowerCase() === "n"){
-//             alert("I knew you had bad taste")
-
-//             }else{
-//                 alert("Nobody ever went pro by being indecisive..")
-//             }
+            }else{
+                alert("Time to code or get off the pot!")
+            }
 
 
-// const show      = prompt("Have you seen the show Black Mirror?");
-//                 console.log(show);            
 
-//         if  (show.toLowerCase() === "yes" || show.toLowerCase() === "y" ){
-//             alert("It\'s good right?! but dont watch it before bed!")
+const header    = prompt("Are the Green Bay Packers your favorite NFL team?");
+            console.log(header);
 
-//         }else if (show.toLowerCase() === "no" || show.toLowerCase() === "n" ){
-//             alert("Don\'t watch it it\'s scary!!")
+        if (header.toLowerCase() === "yes" || header.toLowerCase() === "y"){
+            alert("Well yeah of course they are!")
 
-//         }else{
-//             alert("Invalid answer YO!!")
-//             }
+        }else if (header.toLowerCase() === "no" || header.toLowerCase() === "n"){
+            alert("I knew you had bad taste")
+
+            }else{
+                alert("Nobody ever went pro by being indecisive..")
+            }
+
+
+const show      = prompt("Have you seen the show Black Mirror?");
+                console.log(show);            
+
+        if  (show.toLowerCase() === "yes" || show.toLowerCase() === "y" ){
+            alert("It\'s good right?! but dont watch it before bed!")
+
+        }else if (show.toLowerCase() === "no" || show.toLowerCase() === "n" ){
+            alert("Don\'t watch it it\'s scary!!")
+
+        }else{
+            alert("Invalid answer YO!!")
+            }
             
     
     
-// const tea       = prompt("Do you drink Yerba Mate?");
-//             console.log(tea);
+const tea       = prompt("Do you drink Yerba Mate?");
+            console.log(tea);
 
-//         if  (tea.toLowerCase() === "yes" || tea.toLowerCase() === "y"){
-//              alert("I\'m all hyped out on caffeine!!");
+        if  (tea.toLowerCase() === "yes" || tea.toLowerCase() === "y"){
+             alert("I\'m all hyped out on caffeine!!");
 
-//         }else if (tea.toLowerCase() === "no" || tea.toLowerCase() === "n"){
-//             alert("It gets you hyped on caffeine!!" );
+        }else if (tea.toLowerCase() === "no" || tea.toLowerCase() === "n"){
+            alert("It gets you hyped on caffeine!!" );
 
-//         }else{
-//             alert("This is a yes or no quesion bro");
-//         }
+        }else{
+            alert("This is a yes or no quesion bro");
+        }
 
 
         
-// const music     = prompt("Do you listen to ambient music when falling asleep?");
-//             console.log(music);
+const music     = prompt("Do you listen to ambient music when falling asleep?");
+            console.log(music);
         
-//         if (music.toLowerCase() === "yes" || music.toLowerCase() === "y"){
-//             alert("STILL HYPED ON CAFFEINE!!");
+        if (music.toLowerCase() === "yes" || music.toLowerCase() === "y"){
+            alert("STILL HYPED ON CAFFEINE!!");
 
-//         }else if (music.toLowerCase() === "no" || music.toLowerCase() === "n"){
-//             alert("It\'s relaxing you should try it...");
+        }else if (music.toLowerCase() === "no" || music.toLowerCase() === "n"){
+            alert("It\'s relaxing you should try it...");
 
-//         }else{
-//             alert("I wish a thousand years insomnia upon you.. ")
-//         }
+        }else{
+            alert("I wish a thousand years insomnia upon you.. ")
+        }
